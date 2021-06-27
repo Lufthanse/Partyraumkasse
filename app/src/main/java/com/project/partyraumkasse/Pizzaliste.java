88b3c2da320c4f660abaa148bb86febac210a895
marco <marco.kramhoeller@icloud.com>
2021-06-27 18:49:32 +0200

    Pizzaliste PopUp Marco 27/6/21

--- app/src/main/java/com/project/partyraumkasse/Pizzaliste.java
+++ app/src/main/java/com/project/partyraumkasse/Pizzaliste.java
@@ -2,37 +2,25 @@ package com.project.partyraumkasse;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.EditText;
-import android.widget.TextView;
-
-import com.google.android.material.snackbar.Snackbar;
-
-import java.util.jar.Attributes;
+import android.widget.Button;
 
 public class Pizzaliste extends AppCompatActivity {
-
-    protected EditText Name;
-    protected EditText Pizza;
-    protected EditText Extras;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_pizzaliste);
-        Name = (EditText) findViewById(R.id.inp_namePL);
-        Pizza = (EditText) findViewById(R.id.inp_pizza);
-        Extras = (EditText) findViewById(R.id.inp_Extras);
 
-    }    public void showExtras(View view) {
-
-        if (Extras.getVisibility() == View.INVISIBLE) {
-            Extras.setVisibility(View.VISIBLE);
-        } else {
-            Extras.setVisibility(View.INVISIBLE);
-            Extras.setText("");
-        }
     }
-}
+
+        public void addPizza(View view){
+        startActivity(new Intent(Pizzaliste.this,Pop.class));
+
+        }
+
+    }
+
